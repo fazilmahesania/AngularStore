378a97882c0b4edb2ca850cd92cb795ac4b33f8e
fazilmahesania <43927517+fazilmahesania@users.noreply.github.com>
2023-08-08 14:42:40 -0400

    Completed Product Detail Page

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -8,15 +10,38 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent implements OnInit{
   pageTitle: string = 'Product Detail';
+  imageWidth: number = 30;
+  imageMargin: number = 2;
+  alignCenter: string ='center';
+  sub!: Subscription;
+  errorMessage: string = '';
   product: IProduct | undefined;
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router) { }
+
+  products: IProduct[] = [];
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.pageTitle += `: ${id}`;
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+          this.products = products;
+          this.product = this.products.find(product => product.productId === id);;
+      },
+      error: (err: string) => this.errorMessage = err
+  });
   }
   onBack(): void {
     this.router.navigate(['/products']);
   }
+ 
+  ngOnDestroy() {
+      if (this.sub) {
+      this.sub.unsubscribe();
+      }
+  }
+  onRatingClicked(message: string): void {
+      this.pageTitle = 'Product List: ' + message + ' Rating';
+  }
+
 }
